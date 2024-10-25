2f47a8e9179c3516b1c8edcc4afe258d12554697
tomass <dominanthat@gmail.com>
2024-10-25 20:20:47 +0900

    feat: 해쉬태그와 중간매핑 테이블 레포지토리
    - MeetingHashtagRepository.java : 모임명이나 해쉬태그 명으로 각각을 찾기 위한 정의 함수 추가
    - HashtagRepository.java : 기존에 해쉬태그 존재하는지 확인하기 위한 정의 함수 추가

---
+++ src/main/java/GGUM_Team3/Server/domain/tag/hashtag/repository/MeetingHashtagRepository.java
@@ -0,0 +1,16 @@
+package GGUM_Team3.Server.domain.tag.hashtag.repository;
+
+import GGUM_Team3.Server.domain.tag.hashtag.entity.MeetingHashtagEntity;
+import GGUM_Team3.Server.domain.tempMeeting.entity.TempMeetingEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface MeetingHashtagRepository extends JpaRepository<MeetingHashtagEntity, UUID> {
+    List<MeetingHashtagEntity> findByHashtagEntity_HashtagName(String hashtagName);
+
+    List<MeetingHashtagEntity> findByTempMeetingEntity(TempMeetingEntity tempMeeting);
+}
