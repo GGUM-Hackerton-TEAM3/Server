f5be7851d5be5211fec49a5cd2eb71d49329e4e9
tomass <dominanthat@gmail.com>
2024-10-26 11:15:20 +0900

    feat: User 레포지토리 및 서비스

---
+++ src/main/java/GGUM_Team3/Server/domain/user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package GGUM_Team3.Server.domain.user.repository;
+
+import GGUM_Team3.Server.domain.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String>{
+    Optional<UserEntity> findByEmail(String email);
+    Boolean existsByEmail(String email);
+    UserEntity findByEmailAndPassword(String email, String password);
+
+    Optional<UserEntity> findById(String id);
+
+}
